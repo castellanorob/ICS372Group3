ba2da341af4e2bb493614e8773cac19814682c10
Kean Jaycox <kjaycox3030@gmail.com>
2022-09-18 18:57:33 -0500

    updated DealerList

--- com/gson/DealerList.java
+++ com/gson/DealerList.java
@@ -1,15 +1,62 @@
 package com.gson;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class DealerList {
 	// Need to create a list for dealerList
-	private List<Dealer> dealerList = new ArrayList<Dealer>();
+	private List<Dealer> dealerList;
 	
 	// Add an addDealer method
 	// Add a printFullInventory method
 	
+	//constructor
+	public DealerList(){
+	    this.dealerList = new ArrayList<Dealer>();
+    }
+	
+	//need to solve for Date type
+	public int addVehicle(int dealerID, String type, String manufacturer, String model, String id, int price, Date acqusitionDate) {
+	    
+	    int acquisition;   //what should modifier be, final?
+	    int dealerStatus[] = new int[2];
+	    Iterator<Dealer> dealerListIt = dealerList.iterator();
+
+	    dealerStatus = dealerExist(dealerID);
+	    
+	    //dealerList is null OR dealerID is not in list
+	    if(dealerStatus[0] == 0) {
+            //created dealer
+            Dealer dealer = new Dealer(dealerID, true);
+            //add dealer to list
+            dealerList.add(dealer);
+            //create vehilce
+            Vehicle vehicle = new Vehicle(dealerID,type,manufacturer,model,id,price,acqusitionDate);
+            //add vehicle to newly created dealer
+            dealer.addVehicle(vehicle);
+            //return 2 -> "dealer created, vehicle added to inventory"
+            return 2;
+            
+        //dealer exists
+        } else if(dealerStatus[0] == 1) {
+            //check acq status
+            if(dealerList.get(dealerStatus[1]).getAcquisitionEnabled()) {
+                //acquisition enabled
+                //create vehicle
+                Vehicle vehicle = new Vehicle(dealerID,type,manufacturer,model,id,price,acqusitionDate);
+                dealerList.get(dealerStatus[1]).addVehicle(vehicle);
+                //return 1 -> "vehicle added to dealer inventory"
+                return 1;
+            }else {
+                //return 0 -> "dealer acq disabled"
+                return 0;
+            }
+
+        }
+    }
+
+	//this needs to be modified to fit new dealerExist
 	public void addDealer(Dealer dealer) {
 		if(dealerExist(dealer.getDealerID())) {
 			// already in dealers list. Do nothing.	
@@ -19,21 +66,29 @@ public class DealerList {
 		}
 	}
 
-	private boolean dealerExist(int dealerID) {
-		//taking a boolean varibale and say false
-		boolean isExist = false;
-		// loop through all the dealers that we have already.
-		for (int i = 0; i < dealerList.size(); i++) {
-			// check if any of the dealer have the same ID as given ID.
-			// match, If the dealer ID already exist in the list make isExist true.
-			if (dealerList.get(i).getDealerID() == dealerID) {
-				isExist = true;	
-			}
-		}
-		// return the result.
-		return isExist;
+	private int[] dealerExist(int dealerID) {
+
+		int[] dealerStatus = {0,0};
+
+        if(this.dealerList.isEmpty()) {
+            //if dealerList is empty return 0
+            return dealerStatus;
+        } else {
+            //dealers exist, parse list
+    		for (int i = 0; i < dealerList.size(); i++) {
+    		    //dealer found, return 1 and position
+    			if (dealerList.get(i).getDealerID() == dealerID) {
+    				dealerStatus[0] = 1;
+    				dealerStatus[1] = i;
+    				return dealerStatus;
+    			}
+    		}
+        }
+		// return the result no matter what, default to dealer doesn't exist
+		return dealerStatus;
 	}
 	
+	//could iterate over this instead, not important
 	public void printFullInventory() {
 		System.out.print("Full Inventory");
 		for (int i = 0; i < dealerList.size(); i++) {
@@ -41,6 +96,5 @@ public class DealerList {
 			// That makes the full inventory
 			dealerList.get(i).printInventory();
 		}
-		
 	}
 }

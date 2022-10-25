9d6f49a8cc234ea6b6c95276d9c97acdf0769348
Faiza <faizamohamoud826725@gmail.com>
2022-10-24 23:32:36 -0500

    skeleton of remaining UnitTests

---
+++ Group3_Project/src/test/java/ics372group3/DealerListTest.java
@@ -0,0 +1,14 @@
+package ics372group3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DealerListTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
